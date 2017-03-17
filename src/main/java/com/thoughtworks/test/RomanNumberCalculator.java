c0ab1241a4bc13fdacdba17b90ff53b2339b11dc
freyliis <ania.kurman@gmail.com>
2017-03-17 14:57:16 +0100

    Roman Number Calculator - basic calulcations, without rules for numbers

--- src/main/java/com/thoughtworks/test/RomanNumberCalculator.java
+++ src/main/java/com/thoughtworks/test/RomanNumberCalculator.java
@@ -3,9 +3,56 @@ package com.thoughtworks.test;
 public class RomanNumberCalculator {
     public int calculate(RomanNumber... romanNumbers) {
         int sum = 0;
-        for (RomanNumber romanNumber : romanNumbers) {
+        int repeatedNumber = 1;
+        RomanNumber previousNumber = null;
+        for (int i = 0; i < romanNumbers.length; i++) {
+            RomanNumber currentNumber = romanNumbers[i];
 
-            sum += romanNumber.getValue();
+            if (currentNumber.equals(previousNumber)) {
+                repeatedNumber++;
+                if (repeatedNumber > 3) {
+                    throw new IllegalArgumentException();
+                }
+            } else {
+                repeatedNumber = 1;
+            }
+            if (previousNumber != null && currentNumber.getValue() > previousNumber.getValue()) {
+                sum += (currentNumber.getValue() - 2 * previousNumber.getValue());
+            } else {
+                sum += currentNumber.getValue();
+            }
+            previousNumber = currentNumber;
+        }
+
+//        sum = getSum(sum, lastParsedRomanNumber, romanNumbers);
+        return sum;
+    }
+
+
+    private int getSum(int sum, RomanNumber[] romanNumbers) {
+        RomanNumber lastParsedRomanNumber = null;
+        for (int i = romanNumbers.length - 1; i >= 0; ) {
+            RomanNumber currentNumber = romanNumbers[i];
+            if (lastParsedRomanNumber != null && currentNumber.getValue() < lastParsedRomanNumber.getValue()) {
+                throw new IllegalArgumentException();
+            }
+//            if(romanNumber.getValue().equals())
+            if (i - 1 < 0) {
+                sum += currentNumber.getValue();
+                break;
+            } else {
+                RomanNumber numberBeforeCurrent = romanNumbers[i - 1];
+                if (numberBeforeCurrent.getValue() < currentNumber.getValue()) {
+                    lastParsedRomanNumber = numberBeforeCurrent;
+                    sum += currentNumber.getValue() - numberBeforeCurrent.getValue();
+                    i = i - 2;
+                } else {
+                    lastParsedRomanNumber = currentNumber;
+                    sum += currentNumber.getValue();
+                    i = i - 1;
+                }
+
+            }
         }
         return sum;
     }
--- src/test/java/com/thoughtworks/test/RomanNumberCalculatorTest.java
+++ src/test/java/com/thoughtworks/test/RomanNumberCalculatorTest.java
@@ -4,28 +4,57 @@ import org.junit.Test;
 
 import static com.thoughtworks.test.RomanNumber.I;
 import static com.thoughtworks.test.RomanNumber.V;
+import static com.thoughtworks.test.RomanNumber.X;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class RomanNumberCalculatorTest {
 
     @Test
-    public void shouldAddThreeI() {
+    public void shouldCalculateIIIAs3() {
         RomanNumberCalculator objectUnderTest = new RomanNumberCalculator();
         int result = objectUnderTest.calculate(I, I, I);
         assertThat(result, is(3));
     }
+
     @Test
-    public void shouldAddVAndI() {
+    public void shouldCalculateVIAs6() {
         RomanNumberCalculator objectUnderTest = new RomanNumberCalculator();
         int result = objectUnderTest.calculate(V, I);
         assertThat(result, is(6));
     }
+
     @Test
-    public void shouldSubtractIFromV() {
+    public void shouldCalculateVIIAs7() {
+        RomanNumberCalculator objectUnderTest = new RomanNumberCalculator();
+        int result = objectUnderTest.calculate(V, I, I);
+        assertThat(result, is(7));
+    }
+
+    @Test
+    public void shouldCalculateXXXIIAs32() {
+        RomanNumberCalculator objectUnderTest = new RomanNumberCalculator();
+        int result = objectUnderTest.calculate(X, X, X, I, I);
+        assertThat(result, is(32));
+    }
+
+    @Test
+    public void shouldCalculateIVAs4() {
         RomanNumberCalculator objectUnderTest = new RomanNumberCalculator();
         int result = objectUnderTest.calculate(I, V);
         assertThat(result, is(4));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldThrowAnExceptionForIIV() {
+        RomanNumberCalculator objectUnderTest = new RomanNumberCalculator();
+        objectUnderTest.calculate(I, I, V);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldThrowAnExceptionForMoreThanThreeI() {
+        RomanNumberCalculator objectUnderTest = new RomanNumberCalculator();
+        objectUnderTest.calculate(I, I, I, I);
     }
 
 }
